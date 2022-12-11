10d4f558e4838ed8ea8994291b3da54b6a175442
Joe Reed <joereed.dev@gmail.com>
2022-12-11 13:07:13 +0000

    Update test template

--- templates/day_test.go
+++ templates/day_test.go
@@ -27,3 +27,23 @@ func TestPuzzle1(t *testing.T) {
 func TestSolvePuzzle1(t *testing.T) {
 	fmt.Println("Puzzle 1:", puzzle1(LoadFile("./input.txt")))
 }
+
+// func TestPuzzle2(t *testing.T) {
+// 	tests := []struct {
+// 		input    string
+// 		expected int
+// 	}{
+// 		{
+// 			"./test-input-1.txt",
+// 			0,
+// 		},
+// 	}
+
+// 	for _, test := range tests {
+// 		assert.Equal(t, test.expected, puzzle2(LoadFile(test.input)))
+// 	}
+// }
+
+// func TestSolvePuzzle2(t *testing.T) {
+// 	fmt.Println("Puzzle 2:", puzzle2(LoadFile("./input.txt")))
+// }
