e51bfcf96f6d29f1ed6e728e29cdae4e696c98d8
Joe Reed <joereed.dev@gmail.com>
2022-12-13 18:55:49 +0000

    Rename template files

--- next.sh
+++ next.sh
@@ -3,5 +3,5 @@
 latest=$(ls -d -r day-* | grep -o '[0-9]\+' | head -1)
 next=$(printf "%02d\n" $((${latest#0} + 1)))
 cp -R templates day-$next
-mv day-$next/day.go day-$next/day-$next.go
-mv day-$next/day_test.go day-$next/day-$next\_test.go
+mv day-$next/day.go.tmpl day-$next/day-$next.go
+mv day-$next/day_test.go.tmpl day-$next/day-$next\_test.go
