7bb31dc69f7effbc758e009c0380e622f876a76a
Joe Reed <joereed.dev@gmail.com>
2022-12-14 06:11:23 +0000

    Echo day from next script

--- next.sh
+++ next.sh
@@ -5,3 +5,4 @@ next=$(printf "%02d\n" $((${latest#0} + 1)))
 cp -R templates day-$next
 mv day-$next/day.go.tmpl day-$next/day-$next.go
 mv day-$next/day_test.go.tmpl day-$next/day-$next\_test.go
+echo day-$next
